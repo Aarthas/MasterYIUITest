b097526d62f812b1aad26924ded142aecaf03f47
xuwangcheng <xuwangcheng14@163.com>
2018-11-02 16:06:22 +0800

    update

--- src/main/java/com/dcits/yi/ui/usecase/UseCase.java
+++ src/main/java/com/dcits/yi/ui/usecase/UseCase.java
@@ -10,13 +10,16 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UseCase {
+	//用例名称，必填
 	String name();
 	//为true时执行该用例方法，否则不执行
 	boolean enabled() default true; 
 	//执行失败时是否中断整个测试流程
 	boolean failInterrupt() default false;
-	//标签，在分布式运行时根据标签分组
+	//标签，在分布式运行时根据标签来进行分组
 	String tag() default "default";
+	//暂时无用
 	int retryCount() default 0;
+	//可在此配置一个或者多个浏览器类型，实际测试中，将会按照你配置的顺序依次启动不同类型的浏览器来执行这个测试用例
 	String[] browserType() default {};
 }
