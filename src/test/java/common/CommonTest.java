b8eae5cf5f2afe990934b5a922d3be7d07f7ac45
xuwangcheng <xuwangcheng14@163.com>
2018-11-02 11:09:22 +0800

    稳定更新

--- src/test/java/common/CommonTest.java
+++ src/test/java/common/CommonTest.java
@@ -1,6 +1,9 @@
 package common;
 
+import org.springframework.boot.SpringApplication;
+
 import com.dcits.yi.WebTest;
+import com.dcits.yi.api.Application;
 import com.dcits.yi.ui.report.manage.ZTestReportManager;
 
 
@@ -8,13 +11,13 @@ public class CommonTest {
 	
 	public static void main(String[] args) throws Exception {
 		//实例化WebTest对象，可传入suite文件或者多个测试用例类
-		WebTest test = new WebTest("testsuite");
+		//WebTest test = new WebTest("testsuite");
 		
 		//传入一个或多个测试报告处理器对象
-		test.setReportManagers(new ZTestReportManager());		
+		//test.setReportManagers(new ZTestReportManager());		
 		
 		//常规启动测试
-		test.start();
+		//test.start();
 		
 		//使用seleniumConfig.properties中配置的定时规则
 		//test.startCron();
@@ -26,6 +29,6 @@ public class CommonTest {
 		//System.out.println(JSONUtil.parse(GlobalTestConfig.report).toStringPretty());
 		
 		//启动spring-boot-web，可以调用api获取一些信息或者启动测试
-		//SpringApplication.run(Application.class);
+		SpringApplication.run(Application.class);
 	}
 }
